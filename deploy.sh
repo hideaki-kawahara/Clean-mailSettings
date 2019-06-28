cefbba30112395bc982bda61985b122206e2d2b2
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2019-06-28 20:05:15 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,8 @@ set -eux
 
 export TZ="Asia/Tokyo"
 TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
-
-for TARGET in Clean-mailSettings2.pdf;
-do
-  FILENAME=$(basename $TARGET)
-  curl -X POST -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
-	-D - -H "Dropbox-API-Arg: {\"path\": \"/${FILENAME#.pdf}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" \
-    -H "Content-Type: application/octet-stream" --data-binary @${TARGET} https://content.dropboxapi.com/2/files/upload
-done
+TARGET="Clean-mailSettings2.pdf"
+FILENAME=$(basename $TARGET)
+curl -X POST -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
+-D - -H "Dropbox-API-Arg: {\"path\": \"/${FILENAME#.pdf}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" \
+  -H "Content-Type: application/octet-stream" --data-binary @${TARGET} https://content.dropboxapi.com/2/files/upload
