e5f33fe05e8b2f81b18b4830a3cc6cd511497e78
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2019-06-29 11:24:52 +0900

    update path change

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 TARGET="Clean-mailSettings2.pdf"
 FILENAME=$(basename $TARGET)
 curl -X POST -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
--D - -H "Dropbox-API-Arg: {\"path\": \"/${FILENAME#.pdf}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" \
+-D - -H "Dropbox-API-Arg: {\"path\": \"/Clean-mailSettings/${FILENAME#.pdf}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" \
   -H "Content-Type: application/octet-stream" --data-binary @${TARGET} https://content.dropboxapi.com/2/files/upload
